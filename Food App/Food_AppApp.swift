46ce62c5f3d02ffd383e4e1952be32e61bf334c6
Arzu95 <muhammadarzu3432@gmail.com>
2021-03-04 13:55:40 +0700

    Lesson 4 -Create CartView And FixedProject

--- Food App/Food_AppApp.swift
+++ Food App/Food_AppApp.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
 
 @main
 struct Food_AppApp: App {
-    
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -19,12 +21,12 @@ struct Food_AppApp: App {
     }
 }
 
-
 class AppDelegate: NSObject, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        FirebaseApp.configure()
-        return true
-    }
+    FirebaseApp.configure()
+    return true
+  }
 }
+
--- Food App/View/Home.swift
+++ Food App/View/Home.swift
@@ -4,71 +4,81 @@ struct Home: View {
   
   @StateObject var HomeModel = HomeViewModel()
   
-    var body: some View {
-      ZStack{
-        VStack(spacing: 10){
-          HStack(spacing: 15){
-            
-            Button(action: {
-              withAnimation(.easeIn){HomeModel.showMenu.toggle()}
-            }, label: {
-              Image(systemName: "line.horizontal.3")
-                .font(.title)
-                .foregroundColor(Color.white)
-            })
-            
-            Text(HomeModel.userLocation == nil ? "Location..." : "Deliver to")
-              .foregroundColor(.black)
-            
-            Text(HomeModel.userAddress)
-              .font(.caption)
-              .fontWeight(.heavy)
-              .foregroundColor(.pink)
-            
-              Spacer(minLength: 0)
-          }
-          .padding([.horizontal,.top])
+  var body: some View {
+    ZStack{
+      VStack(spacing: 10){
+        HStack(spacing: 15){
           
-          Divider()
+          Button (action: {
+            withAnimation(.easeIn){HomeModel.showMenu.toggle()}
+          }, label: {
+            Image(systemName: "line.horizontal.3")
+              .font(.title)
+              .foregroundColor(Color.pink)
+          })
           
-          HStack(spacing: 15){
-                Image(systemName: "magnifyingglass")
-                  .font(.title)
-                  .foregroundColor(.gray)
-            TextField("Search", text: $HomeModel.search)
-          }
-          .padding(.horizontal)
-          .padding(.top, 10)
+          Text(HomeModel.userLocation == nil ? "Locating..." : "Deliver to")
+            .foregroundColor(.black)
           
-          Divider()
+          Text(HomeModel.userAddress)
+            .font(.caption)
+            .fontWeight(.heavy)
+            .foregroundColor(.pink)
           
+          Spacer(minLength: 0)
+        }
+        .padding([.horizontal,.top])
+        
+        Divider()
+        
+        HStack(spacing: 15){
+          
+          Image(systemName: "magnifyingglass")
+            .font(.title)
+            .foregroundColor(.gray)
+          
+          TextField("Search", text: $HomeModel.search)
+          
+        }
+        .padding(.horizontal)
+        .padding(.top, 10)
+        
+        Divider()
+        
+        //Spacer() diganti sama scrollview
+        if HomeModel.items.isEmpty{
+          Spacer()
+          ProgressView() //loading
+          Spacer()
+        } else {
           ScrollView(.vertical, showsIndicators: false, content: {
             VStack(spacing: 25){
               ForEach(HomeModel.filtered){ item in
                 
                 ZStack(alignment: Alignment(horizontal: .center, vertical: .top), content: {
-                  
                   ItemView(item: item)
                   
                   HStack{
-                    Text("FREE DELIVERY")
+                    Text("Free Delivery")
                       .foregroundColor(.white)
                       .padding(.vertical, 10)
                       .padding(.horizontal)
                       .background(Color.pink)
-                    
+        
                     Spacer(minLength: 0)
                     
-                    Button(action: {}, label: {
-                      Image(systemName: "plus")
+                    Button(action: {
+                      HomeModel.addToCart(item: item)
+                    }, label: {
+                      Image(systemName: item.isAdded ? "checkmark" : "plus")
                         .foregroundColor(.white)
                         .padding(10)
-                        .background(Color.pink)
+                        .background(item.isAdded ? Color.green : Color.pink)
                         .clipShape(Circle())
                     })
                   }
                   .padding(.trailing, 10)
-                  .padding(.top,10)
+                  .padding(.top, 10)
                 })
                 .frame(width: UIScreen.main.bounds.width - 30)
               }
@@ -76,46 +86,47 @@ struct Home: View {
             .padding(.top, 10)
           })
         }
+      }
+      
+      HStack{
+        Menu(homeData: HomeModel)
+          .offset(x: HomeModel.showMenu ? 0 : -UIScreen.main.bounds.width / 1.6)
         
-        HStack{
-          Menu(homeData: HomeModel)
-            .offset(x: HomeModel.showMenu ? 0 : -UIScreen.main.bounds.width / 1.6)
-          
-          Spacer(minLength: 0)
-        }
-        .background(Color.black.opacity(HomeModel.showMenu ? 0.3 : 0).ignoresSafeArea()
-                      .onTapGesture(perform: {
-                        withAnimation(.easeIn){HomeModel.showMenu.toggle()}
-                      })
-        )
-        if HomeModel.noLocation {
-          Text("Please Enable Location Access In Settings To Further Move On !!")
-            .foregroundColor(.black)
-            .frame(width: UIScreen.main.bounds.width - 100, height: 120)
-            .background(Color.white)
-            .cornerRadius(10)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.black.opacity(0.3).ignoresSafeArea())
+        Spacer(minLength: 0)
+      }
+      .background(Color.black.opacity(HomeModel.showMenu ? 0.3 : 0).ignoresSafeArea()
+                    .onTapGesture(perform: {
+                      withAnimation(.easeIn){HomeModel.showMenu.toggle()}
+                    })
+      )
+      if HomeModel.noLocation {
+        Text("Please Enable Location Access In Settiings To Further Move On !!")
+          .foregroundColor(.black)
+          .frame(width: UIScreen.main.bounds.width - 100, height: 120)
+          .background(Color.white)
+          .cornerRadius(10)
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+          .background(Color.black.opacity(0.3).ignoresSafeArea())
+      }
+      
+    }
+    .onAppear(perform: {
+      HomeModel.locationManager.delegate = HomeModel
+    })
+    
+    .onChange(of: HomeModel.search, perform: { value in
+      
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+        if value == HomeModel.search && HomeModel.search != "" {
+          //search data
+          HomeModel.filterData()
         }
       }
-      .onAppear(perform: {
-        HomeModel.locationManager.delegate = HomeModel
-      })
-      .onChange(of: HomeModel.search, perform: { value in
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
-          if value == HomeModel.search && HomeModel.search != ""{
-            HomeModel.filterData()
-          }
-        }
-        if HomeModel.search == ""{
-          withAnimation(.linear) { HomeModel.filtered = HomeModel.items }
-        }
-      })
-    }
-}
-
-struct Home_Previews: PreviewProvider {
-    static var previews: some View {
-        Home()
-    }
+      
+      if HomeModel.search == "" {
+        //reset all data
+        withAnimation(.linear){ HomeModel.filtered = HomeModel.items}
+      }
+    })
+  }
 }
